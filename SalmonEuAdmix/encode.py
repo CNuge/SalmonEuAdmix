20b256668594c291e212af0e0f64350cc29fda6e
Cam Nugent <camnugent@gmail.com>
2022-06-28 09:46:03 -0300

    setting up repo

---
+++ SalmonEuAdmix/encode.py
@@ -0,0 +1,131 @@
+import pickle
+import numpy as np
+import pandas as pd
+
+def readPedMap_tsv_fmt(ped_file, map_file = "", headers = False):
+    
+    col_names = ["chromosome", "snp" , "genetic_distance", "physical_distance"]
+
+    if headers == True:
+        map_data = pd.read_csv(map_file, skiprows = 0, names = col_names, sep = '\t')
+        
+        header_data = ['#family', 'individual', 'sire', 'dam', 'sex', 'pheno']
+        snp_columns = list(map_data['snp'].values)
+        header_data.extend(snp_columns)
+
+        snp_data = pd.read_csv(ped_file, skiprows = 0, names = header_data, sep = '\t')
+        return snp_data, snp_columns
+        
+    else:
+        
+        map_data = pd.read_csv(map_file, names = col_names, sep = '\t')
+        
+        header_data = ['#family', 'individual', 'sire', 'dam', 'sex', 'pheno']
+        snp_columns = list(map_data['snp'].values)
+        header_data.extend(snp_columns)
+        
+        snp_data = pd.read_csv(ped_file, names = header_data, sep = '\t')
+        return snp_data, snp_columns
+
+
+
+def get_unique_alleles(gt_count_dict):
+    """Determine the major and minor alleles for the given marker."""
+    unique_alleles = {}
+    for x in gt_count_dict.keys():
+        for a in x.split(" "):
+            if a in unique_alleles.keys():
+                unique_alleles[a] = unique_alleles[a] + gt_count_dict[x]
+            else:
+                unique_alleles[a] = gt_count_dict[x]
+    if len(unique_alleles) > 2:
+        raise ValueError("SNP is not biallelic")
+    if len(unique_alleles) == 1:
+        raise ValueError("SNP is homozygous")
+    k1, k2 = unique_alleles.keys()
+    if unique_alleles[k1] >= unique_alleles[k2]:
+        return k1, k2
+    else:
+        return k2, k1
+
+
+def calc_mode(snp_arr):
+    """get the most common value in the array of SNP values. For imputing missing info"""
+    vals, counts = np.unique(snp_arr, return_counts=True)
+    index = np.argmax(counts)
+    return vals[index]
+
+
+
+#  snp_arr = snp_data[x].values
+#' Homozygous for major allele encoded as 0, heterozygoous = 1, Homozygous minor allele = 2
+#' method - make it so you can do one hot, dosage, or presence/absence. currently just dosage
+#' missing_data - can put in the mode or NA
+def dosage_encode_snps(snp_arr, missing_val = "0 0", replace_missing_method = "mode", 
+                            record_snps = False, known_pq = False):
+
+    if replace_missing_method == "mode" :
+        mode_gt = calc_mode(snp_arr)
+        if mode_gt == missing_val:
+            raise ValueError("most common allele is a missing genotype!")
+
+        snp_arr[snp_arr == missing_val] = mode_gt
+    
+    if known_pq == True:
+        #use the known major and minor alleles
+        if len(known_pq) != 2:
+            raise ValueError("tuple of known_pq must be made of two and only two characters.")
+        p, q = known_pq
+
+    else:
+        #determine the major and minor alleles
+        vals, counts = np.unique(snp_arr, return_counts=True)
+        gt_count_dict = {k : v for k, v in zip(vals, counts)}
+
+        p, q = get_unique_alleles(gt_count_dict)
+ 
+    encodings = {f"{p} {p}" : 0,
+                    f"{p} {q}" : 1,
+                    f"{q} {p}" : 1,
+                    f"{q} {q}" : 2}
+
+    encoded_data = [encodings[x] for x in snp_arr] 
+
+    if record_snps == True:
+        outdict = {'p' : p , 'q' : q}
+        return encoded_data, outdict
+
+    return encoded_data, None
+
+
+def encode_ped(snp_data, snp_columns, get_alleles = False, encoding_dict = None):
+    """ Take a string format PED and turn it into dosage encoding."""
+
+    #encode with known major and minor alleles
+    if encoding_dict is not None:
+        #x = snp_columns[3]
+        for x in snp_columns:
+            pq_info = encoding_dict[x]
+            #print(x)
+            snp_data[x], _ = dosage_encode_snps(snp_data[x].values, known_pq = pq_info)
+
+        return snp_data, None
+
+    #calculate major and minor alleles from scratch, encode and return the major and minor dictonary
+    elif get_alleles == True:
+        allele_info = {}
+        for x in snp_columns:
+            #print(x)
+            snp_data[x], snp_dict = dosage_encode_snps(snp_data[x].values, record_snps = True)
+            allele_info[x] = snp_dict
+
+        return snp_data, allele_info
+
+    #calculate major and minor alleles from scratch, encode but don't save the major and minor info
+    else:
+        #x = snp_columns[3]
+        for x in snp_columns:
+            #print(x)
+            snp_data[x], _ = dosage_encode_snps(snp_data[x].values)
+        return snp_data, None
+
