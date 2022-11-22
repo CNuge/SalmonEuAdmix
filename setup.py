9d338892e04f2517aeab56cb27a25c56dc7b1f4d
Cam Nugent <camnugent@gmail.com>
2022-11-22 15:19:18 -0400

    pypi update

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ estimations provided by a complete admixture analysis with greater than 98 perce
 
 setup(
 	name = 'SalmonEuAdmix',
-	version = '1.0.4',
+	version = '1.0.5',
 	author = 'Cam Nugent',
 	author_email = 'Cameron.Nugent@dfo-mpo.gc.ca',
 	url = 'https://github.com/CNuge/SalmonEuAdmix',
